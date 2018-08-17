b4eec0a8a08421d28fd084b51b7616b1ba210a3a
Melissa Real <melissa.real@hotmail.com>
2018-08-17 19:14:11 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,16 +12,20 @@ def create_project_hash
 
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+
+
+  def create_project_hash
   projects = {}
-   
-    # Iterate through the projects
-    kickstarter.css("li.project.grid_4").each do |project|
-      projects[project] = {}
-    end
-   
-    # return the projects hash
+
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end
+
+  # return the projects hash
     projects
   end
 
-  
+
+
 end
